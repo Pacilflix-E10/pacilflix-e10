6779bd32386afa958b1ca1b9a4b6b5c369afedf4
IrPanTech <irfannkamill@gmail.com>
2024-05-01 00:02:43 +0700

    feat: CRU Langganan

--- src/components/modules/LanggananModule/index.tsx
+++ src/components/modules/LanggananModule/index.tsx
@@ -6,14 +6,14 @@ import { PilihPaketLain } from "./sections/PilihPaketLain";
 import { RiwayatTransaksi } from "./sections/RiwayatTransaksi";
 
 export const LanggananModule = () => {
-  return (
-    <section className="flex flex-col gap-6 px-4 md:px-10 py-3 md:py-5 mt-[80px]">
-      <h1 className="text-center font-bold text-[24px] md:text-[32px]">
-        HALAMAN KELOLA LANGGANAN
-      </h1>
-      <ActivePacket />
-      <PilihPaketLain />
-      <RiwayatTransaksi />
-    </section>
-  );
+    return (
+        <section className="flex flex-col gap-6 px-4 md:px-10 py-3 md:py-5">
+            <h1 className="text-center font-bold text-[24px] md:text-[32px]">
+                HALAMAN KELOLA LANGGANAN
+            </h1>
+            <ActivePacket />
+            <PilihPaketLain />
+            <RiwayatTransaksi />
+        </section>
+    );
 };
