1f5b5eeb8c01b2ba55b5bf772e73ce6979505891
Maurice Yang <mauriceyangmy28@gmail.com>
2024-05-19 23:19:37 +0700

    fix: Fixing suspense issue

--- src/app/tayangan/page.tsx
+++ src/app/tayangan/page.tsx
@@ -1,5 +1,10 @@
 import { TayanganModule } from "@/components/modules/TayanganModule";
+import { Suspense } from "react";
 
-const TayanganPage = () => <TayanganModule />;
+const TayanganPage = () => (
+    <Suspense fallback={<p>Loading...</p>}>
+        <TayanganModule />
+    </Suspense>
+);
 
 export default TayanganPage
